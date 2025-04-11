f1aa059abaad5f1ceb413c669320159238f972e1
Pedro Marques Esteves <pedromaresteves@gmail.com>
2025-04-11 19:09:54 +0000

    Hash

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ cracked_password2 = password_cracker.crack_sha1_hash(
     "dcc466796201f7232b22a03781110a8871fd038c", True)
 print(cracked_password2)
 
+cracked_password3 = password_cracker.crack_sha1_hash(
+    "b305921a3723cd5d70a375cd21a61e60aabb84ec")
+print(cracked_password3)
+
+cracked_password4 = password_cracker.crack_sha1_hash(
+    "53d8b3dc9d39f0184144674e310185e41a87ffd5", True)
+print(cracked_password4)
+
+cracked_password5 = password_cracker.crack_sha1_hash(
+    "da5a4e8cf89539e66097acd2f8af128acae2f8ae", True)
+print(cracked_password5)
+
+cracked_password6 = password_cracker.crack_sha1_hash(
+    "ea3f62d498e3b98557f9f9cd0d905028b3b019e1", True)
+print(cracked_password6)
+
 # Run unit tests automatically
 main(module = "test_module", exit = False)
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,28 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    read_top_passwords = open('top-10000-passwords.txt', 'r')
+    top_passwords_arr = read_top_passwords.read().split('\n')
+    read_salts = open('known-salts.txt', 'r')
+    salts = read_salts.read().split('\n')
+    for word in top_passwords_arr:
+        if use_salts:
+            for salt in salts:
+                prepended_hashed_word = hash_word(salt + word)
+                apended_hashed_word = hash_word(word + salt)
+                if prepended_hashed_word == hash:
+                    return word
+                if apended_hashed_word == hash:
+                    return word
+        else:
+            hashed_word = hash_word(word)
+            if hashed_word == hash:
+                return word
+    return "PASSWORD NOT IN DATABASE"
+
+def hash_word(word):
+    sha1 = hashlib.sha1()
+    sha1.update(word.encode())
+    hashed_hex = sha1.hexdigest()
+    return hashed_hex
+
